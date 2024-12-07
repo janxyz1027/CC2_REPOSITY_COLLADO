36e3cf735246586b1e2c6e706cd96c4b29e4cff8
janxyz1027 <john.collado094@gmail.com>
2024-12-07 18:11:51 +0800

    Create Lab2.java

---
+++ Lab2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculating;
+
+/**
+ *
+ * @author janxyz
+ */
+public class Lab2 {
+
+    public static void main(String[] args) {
+        
+        float gardenLength = 30.3f;
+        float gardenWidth = 20.2f;
+        
+        System.out.println("The Legth of the garden is: "+gardenLength+" meters");
+        System.out.println("The Width of the garden is: "+gardenWidth+" meters");
+        System.out.println("The Area of the garden is: "+gardenLength*gardenWidth+" meters");
+        System.out.println("The Perimeter of the garden is: "+2*(gardenLength+gardenWidth)+" meters");
+        
+        
+    }
+}
