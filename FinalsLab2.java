37d03f7d7ac989ca697c8f2cc4ed3801402d0f0a
janxyz1027 <john.collado094@gmail.com>
2024-12-07 18:37:13 +0800

    Update FinalsLab2.java

--- FinalsLab2.java
+++ FinalsLab2.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
+//COLLADO JOHN FRANK
+//CITCS 1N A
 package com.mycompany.finalslab2;
-
-/**
- *
- * @author johnc
- */
 public class FinalsLab2 {
 
     public static void main(String[] args) {
