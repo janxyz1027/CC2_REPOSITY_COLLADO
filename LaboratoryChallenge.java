0d41f769c8a38ecc2509a7f47a995984fd75b622
janxyz1027 <john.collado094@gmail.com>
2024-12-07 18:10:36 +0800

    Create LaboratoryChallenge.java

---
+++ LaboratoryChallenge.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorychallenge;
+
+/**
+ *
+ * @author janxyz
+ */
+public class LaboratoryChallenge {
+
+    public static void main(String[] args) {
+        System.out.println("\t\t\tJanxy\"s Store");//used "\t" and "\"" to center and include quotation marks in the store's name
+        System.out.println("\t\t\tCity Camp Central, Baguio City");
+        System.out.println("\t\t\tContact number: 09694697884");
+        //Store Information
+        System.out.println("_______________________________________________________________________");
+        System.out.println("\t\t\t\tINVOICE");//used "\t" and "\"" to center the INVOICE
+        System.out.println("_______________________________________________________________________");
+        //
+        System.out.print("\nReference No: 123-123-123");//used "\n" to make a new line or space above the reference no.
+        System.out.println("\t\t\tDate: September 3, 2024");
+        System.out.println("Account No: 321-321-321");
+        System.out.println("Paid to: Unknown");
+        System.out.println("________________________________________________________________________");
+        //Itemized list
+        System.out.print("ITEM \t\tQuantity \t\tUnit Price \t\tTotal");//used "\t" to align the item names quantities and prices
+        System.out.println("\n________________________________________________________________________");
+        System.out.println("Milk\t\t1\t\t\t90\t\t\t90");
+        System.out.println("________________________________________________________________________");
+        System.out.println("Bread\t\t2\t\t\t50\t\t\t100");
+        System.out.println("________________________________________________________________________");
+        System.out.println("Egg\t\t5\t\t\t10\t\t\t50");
+        System.out.println("________________________________________________________________________");
+        //Total amount
+        System.out.println("\t\t\t\t\t\tTax(0%)\t\t0");
+        System.out.println("\t\t\t\t\t\tVat(0%)\t\t0");
+        System.out.print("\nTotal Amount Due:\t\t\t\t\t\t240");//used print to combine the Total Amount Due and "240" in the same line
+        System.out.print("");
+        System.out.println("\n________________________________________________________________________");
+        //Additional Notes
+        System.out.println("\n\t\t\tTHANK YOU FOR PURCHASING");
+        System.out.println("");
+        System.out.println("________________________________________________________________________");
+    }
+}
