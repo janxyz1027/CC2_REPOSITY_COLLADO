b76b4e84dfb1a6ba7c16999f5e8d54335aeefe1d
janxyz1027 <john.collado094@gmail.com>
2024-12-07 18:37:48 +0800

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
+//COLLADO JOHN FRANK
+//CITCS 1N A
 package com.mycompany.lab1;
-
-/**
- *
- * @author janxyz
- */
 public class Lab1 {
 
     public static void main(String[] args) {
